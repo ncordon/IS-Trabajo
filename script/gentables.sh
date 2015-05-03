b5ef3f7a3bb0947965ef31a49c4c15341203016f
NCordon <nacho.cordon.castillo@gmail.com>
2015-05-03 06:22:40 +0200

    Adds tablas.tex

--- script/gentables.sh
+++ script/gentables.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-HDD="Maxtor6K040L0 ST34321A ST320410A ST320413A ST3320613AS WDBUZG0010BBK"
+HDD="Maxtor6K040L0 ST34321A ST320410A ST320413A ST3320613AS WD800JD WDBUZG0010BBK"
 FS="ext4 fat32 ntfs"
-PARAMS="wMB w avgrqsz avgqusz util"
+PARAMS="wMB w avgrqsz avgqusz"
 declare -A TEXT=(["wMB"]="MB/s transferidos"
 		 ["w"]="Media pet. escritura completadas por segundo"
 		 ["avgrqsz"]="Tamaño medio peticiones escritura(KB)"
-		 ["avgqusz"]="Longitud media de la cola de escritura"
-		 ["util"]="Ancho de banda para el dispositivo")
+		 ["avgqusz"]="Longitud media de la cola de escritura")
 
+declare -A IDS=(["Maxtor6K040L0"]="M1"
+		["ST34321A"]="S1"
+		["ST320410A"]="S2"
+		["ST320413A"]="S3"
+		["ST3320613AS"]="S4"
+		["WD800JD"]="WD1"
+		["WDBUZG0010BBK"]="WD2")
+
+SIZE=5
 FIRST_IT="wMB"
-DATA_DIR=./data
+DATA_DIR="../data"
 # Floating point numbers precision
 PREC=3
 
 for hdd in $HDD
 do
-    echo '\begin{longtable}{|l|l|l|l|}'
-    echo '\caption{Tabla de resultados para [M1]}\\'
+    echo '\begin{longtable}{|>{\centering}m{5cm}|c|c|c|}'
+    echo -n "\caption{Tabla de resultados para [${IDS[$hdd]}]}"
+    echo '\\'
     echo '\hline'
 
     
@@ -35,16 +44,22 @@ do
 	fi
 
 
-	for j in `seq 1 5`
+	for j in `seq 1 ${SIZE}`
 	do
 	    echo -n "$j copias simultáneas"
 
 	    for fs in $FS
 	    do
-		data=$(cat $DATA/$fs/$hdd/log-$j/averages |
+		data=$(cat $DATA_DIR/$fs/$hdd/log-$j/averages |
 		      grep -o "$p:.*" | egrep -o "[[:digit:]]+\.[[:digit:]]+")
 		echo -n " & "
-                printf "%.*f" $PREC "$data"
+		
+		if [[ ! -z "$data" ]]
+		then
+                    printf "%.*f" $PREC "$data"
+		else
+		    echo "-"
+		fi
 	    done
 
 	    echo '\\'
--- script/gentex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TEX_DIR=../tex
-
-source-highlight -f latex -s sh -i geninfo.sh -o $TEX_DIR/geninfo.tex
-source-highlight -f latex -s python -i iostat_plotter_v3.py -o $TEX_DIR/iostat_plotter_v3.tex
-
